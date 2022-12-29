c66e71aa873496f3c1d9a76a3e4b0a359b0a4167
greybrunix <a96544@alunos.uminho.pt>
2022-12-29 20:11:51 +0000

    Insert and Append
    Successfuly allows the printing inserting and appending lines

--- src/tesrc.c
+++ src/tesrc.c
@@ -1,3 +1,9 @@
+/*
+   A modal line editor based on ed for the personal use of the creator
+   and for the pedagogic purpose of creating a vi like text editor in 
+   the future.
+   Bruno Giao 2022/2023
+*/
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +21,7 @@
 int
 main(int argc,char**argv)
 {
+  /* argc = 1 || argc = 2 */
   u_int32_t LINS, COLS;
   u_int32_t curr_line;
   u_int32_t curr_col_max[128];
@@ -25,7 +32,7 @@ main(int argc,char**argv)
 
 
   /* Atributions */
-  LINS = 128; COLS = 180;
+  LINS = 128; COLS = 120;
   curr_line = 0; curr_line_max = 0;
   normal = malloc(64*sizeof(char));
   buffer = (char**) malloc(LINS*sizeof(char *));
@@ -45,7 +52,8 @@ main(int argc,char**argv)
     }
     else flag_no_file = 0;
   }
-  if (argc > 2) flag_err = 1; /* set so it is only ed and file name */
+  if (argc > 2) flag_err = 1; /* set so it is only editor name
+                                 and file name */
 
   /* Code Logic */
   if (flag_err == 0)
@@ -57,51 +65,66 @@ main(int argc,char**argv)
       {
         while (mode == NOR)
         {
-          read(0, normal, 64);
-          if (*normal == 'q') mode = OUT; /* exit command */
-          else if (*normal == 'i') mode = INS;
-          else if (*normal == '+')
+          read(0, normal, COLS);
+          switch(*normal)
           {
-            if (curr_line < curr_line_max)
-              curr_line++; /* if > curr_max */
-            else write(1,"?\n",2);
+            case 'q': mode = OUT; break;/* exit command */
+            case 'i': mode = INS; break;/* switch to insert mode */
+            case 'a': mode = INS; 
+                      curr_line_max++; 
+                      curr_line++;
+                      if (*(*buffer + curr_line-1) == '\0')
+                        *(*buffer + curr_line-1) = '\n';
+                      break;
+            case '+': if (curr_line < curr_line_max)
+                        curr_line++; /* if > curr_max */
+                      else write(1,"?\n",2);
+                      break;
+            case '-': if (curr_line > 0)
+                        curr_line--; /* if < 0 */
+                      else write(1,"?\n",2);
+                      break; 
+            case 'p': if (buffer[curr_line][0] == '\0') write(1,"?\n",2);
+                      else write(1,buffer[curr_line], curr_col_max[curr_line]);
+                      break;
+            case 'w': if (flag_no_file) write(1,"?\n",2);
+                      else for (i = 0;
+                                i < curr_line_max;
+                                write(fd_file, buffer[i], curr_col_max[i]),
+                                i++
+                                ); /* the correct functioning of this 
+                                      code is dependant
+                                      on the correct opening and reading 
+                                      of file to buffer */
+                      break;
+            case 'e': fd_file = open((normal+2), O_CREAT|O_WRONLY, 777); 
+                      if (fd_file < 0) write(1,"?\n",2);
+                      else flag_no_file = 0;
+                      /* Requires passing the contents of the file
+                         to the buffer */
+                      break;
+            default: write(1, "?\n",2);
           }
-          else if (*normal == '-')
-          {
-            if (curr_line > 0)
-              curr_line--; /* if < 0 */
-            else write(1,"?\n",2);
-          }
-          else if (*normal == 'p')
-          {
-            if (buffer[curr_line][0] == '\0') write(1,"?\n",2);
-            else write(1,buffer[curr_line],(int) COLS);
-          }
-          else if (*normal == 'w')
-          {
-            if (flag_no_file) write(1,"?\n",2);
-            else for (i = 0;
-                      i < curr_line_max;
-                      write(fd_file, buffer[i], curr_col_max[i]),i++
-                     );
-          }
-          else write(1, "?\n",2);
-
-          *normal = '\0'; /* reset the normal buffer */
-        }
         *normal = '\0'; /* reset the normal buffer */
+        }
       }
       if (mode == INS)
       {
+        if (**(buffer + curr_line) != '\0')
+        for (i=0; i<curr_col_max[curr_line];i++)
+        *(*(buffer + curr_line) + i) = '\0';
         bytes_read = read(0,buffer[curr_line],(int) COLS);
+        *(*(buffer + curr_line)+bytes_read) = '\0';
+        /*printf("%ld\n", bytes_read);
+        fflush(stdin);*/
         curr_col_max[curr_line] = bytes_read;
-        if (curr_line == curr_line_max) curr_line_max++;
         mode = NOR;
       }
 
     }
   }
   free(normal);
+  for (i=0; i<LINS;free(buffer[i++]));
   free(buffer);
   return flag_err;
 }
